1988fd1429b744256edb538d40800975b24bf195
MeesPos <meespostma_@hotmail.com>
2022-11-29 09:14:21 +0100

    Added basic middleware

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,9 +4,13 @@ import { useSession } from "next-auth/react";
 
 
 const Dashboard: NextPage = () => {
-	const { status, data } = useSession();
+	const { data } = useSession();
 
     console.log(data);
+
+    if (!data) {
+        return <p>Access denied</p>
+    }
 
     return <h1>HOI</h1>;
 }
