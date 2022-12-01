440a96143aafe5018df088fdecaa5528cbf0b6c7
MeesPos <meespostma_@hotmail.com>
2022-12-01 09:41:56 +0100

    Added forgot password styling

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -26,22 +26,31 @@ const ForgotPassword: NextPage = () => {
         }
     }
 
-    return <div>
-        <form onSubmit={handleSubmit}>
-            {successMessage ? <p>{successMessage}</p> : null}
+    return <div className="h-screen w-screen bg-gray-100 flex justify-center">
+        <form onSubmit={handleSubmit} className="bg-white m-auto w-3/12 p-8 rounded-xl">            
+            <h1 className="text-center text-4xl font-bold mb-6">Marjolein <br /> knip knip</h1>
             
-            <input name="email"
-                value={userInfo.email}
-                onChange={({ target }) =>
-                    setUserInfo({
-                        ...userInfo,
-                        email: target.value
-                    })
-                }
-            />
+            {successMessage ? <p className="text-green-600 mb-6">{successMessage}</p> : null}
 
-            <button type="submit">
-                Login
+            <div className="flex flex-col">
+                <label htmlFor="email">E-mailadres</label>
+
+                <input name="email"
+                    id="email"
+                    className="border-2 border-gray-200 py-2 pl-2 mt-1 rounded-md"
+                    value={userInfo.email}
+                    onChange={({ target }) =>
+                        setUserInfo({
+                            ...userInfo,
+                            email: target.value
+                        })
+                    }
+                />
+            </div>
+            
+
+            <button type="submit" className="bg-indigo-500 w-full py-3 text-white text-md font-bold rounded-md mt-8">
+                Verstuur reset linkx
             </button>
         </form>
     </div>
