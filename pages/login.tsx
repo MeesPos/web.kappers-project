85194ff9c02bc97dde2cc0a1aa5124f08050d5c9
MeesPos <meespostma_@hotmail.com>
2022-12-01 09:41:46 +0100

    Added login styling

--- pages/login.tsx
+++ pages/login.tsx
@@ -30,33 +30,50 @@ const Login: NextPage = () => {
         }
     }
 
-    return <div>
-        <form onSubmit={handleSubmit}>
-            {error ? <p>{error}</p> : null}
+    return <div className="h-screen w-screen bg-gray-100 flex justify-center">
+        <form onSubmit={handleSubmit} className="bg-white m-auto w-3/12 p-8 rounded-xl">
+            <h1 className="text-center text-4xl font-bold mb-6">Marjolein <br /> knip knip</h1>
 
-            <input name="email" 
-                   value={userInfo.email}
-                   onChange={({ target }) => 
-                    setUserInfo({
-                        ...userInfo,
-                        email: target.value
-                    })
-                }
-            />
+            {error ? <p className="text-red-500 mb-6">{error}</p> : null}
 
-            <input name="password"
-                   type="password"
-                   value={userInfo.password}
-                   onChange={({ target }) =>
-                    setUserInfo({
-                        ...userInfo,
-                        password: target.value
-                    })
-                }
-            />
+            <div className="flex flex-col">
+                <label htmlFor="email">E-mailadres</label>
 
-            <button type="submit">
-                Login
+                <input name="email"
+                    id="email" 
+                    className="border-2 border-gray-200 py-2 mt-1 rounded-md pl-2"
+                    value={userInfo.email}
+                    onChange={({ target }) => 
+                        setUserInfo({
+                            ...userInfo,
+                            email: target.value
+                        })
+                    }
+                />
+            </div>
+
+            <div className="flex flex-col mt-8">
+                <label htmlFor="password">Wachtwoord</label>
+
+                <input name="password"
+                    type="password"
+                    className="border-2 border-gray-200 py-2 mt-1 rounded-md pl-2"
+                    value={userInfo.password}
+                    onChange={({ target }) =>
+                        setUserInfo({
+                            ...userInfo,
+                            password: target.value
+                        })
+                    }
+                />
+            </div>
+
+            <div className="text-right mt-1">
+                <a href="/forgot-password" className="text-indigo-500 font-medium">Wachtwoord vergeten?</a>
+            </div>
+
+            <button type="submit" className="bg-indigo-500 w-full py-3 text-white text-md font-bold rounded-md mt-4">
+                Inloggen
             </button>
         </form>
     </div>
