85b2c91741b27469a51840e4f9ca6d0e464ee2af
sanderk12 <118729491+sanderk12@users.noreply.github.com>
2022-12-19 09:52:20 +0100

    aanpassingen code
    verandering van breaklines
    verandering van css naar tailwind sommige dingen nog
    onnodige bestanden weggehaald

--- components/Reservering2.tsx
+++ components/Reservering2.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Hoofdpagina from "./Hoofdpagina";
   
-  const handleClick = () => {
-    var id=document.getElementById("eerstebehandeling");
+  const handleClick = (behandeling: string) => {
+    localStorage.setItem('behandeling', behandeling);
 
     location.href = '/kapperselecteren';
   }
 
-  const handleClick2 = () => {
-    var id=document.getElementById("tweedeKapper");
+  const handleClick2 = (behandeling: string) => {
+    localStorage.setItem('behandeling', behandeling);
 
     location.href = '/kapperselecteren';
   }
@@ -16,43 +16,25 @@ import Hoofdpagina from "./Hoofdpagina";
 function Reservering2(){
     return(
         <>
-        ;<div>
+        <div>
   <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
   <title>Reservering maken</title>
-  <div className="achtergrondreservering m-auto p-5 bg-white">
-    <div className="links items-center">
-      <img className="ml-[40%]" src="fotos/Group1.png" width="80px" />
-      <br />
-      <br />
-      <br />
-      <br />
-      <h1 className="text-indigo-500 text-2xl font-bold p-2">Selecteer behandeling</h1>
-      <h4 className="text-indigo-500 font-normal text-gray-500">
+  <div className="bg-auto bg-slate-400">
+  <div className="w-[1200px] h-[800px] rounded-3xl m-auto p-5 z-50 bg-white absolute top-0 left-1/4">
+    <div className="float-left p-2 bg-white text-center w-[440px] h-[700px] items-center">
+      <img className="relative top-[30px] ml-[40%]" src="fotos/Group1.png" width="80px" />
+      <h1 className="relative top-[125px] text-indigo-500 text-2xl font-bold p-2">Selecteer behandeling</h1>
+      <h4 className="relative top-[125px] text-indigo-500 font-normal text-gray-500">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat
       </h4>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <h1 className="text-2xl p-2">Vragen?</h1>
-      <h4 className="text-gray-500">bel (020) 040210 of stuur een email naar mail@kapper-marjolein.nl</h4>
+      <h1 className="relative top-[425px] text-indigo-500 font-bold text-2xl p-2">Vragen?</h1>
+      <h4 className="relative top-[425px] font-normal text-gray-500">bel (020) 040210 of stuur een email naar mail@kapper-marjolein.nl</h4>
     </div>
     <div className="p-5 width-[600px] height-[780px] ml-[40%] ">
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-indigo-500 font-bold text-2xl font-bold">
         Selecteer behandeling
         <br />
         <br />
@@ -61,9 +43,9 @@ function Reservering2(){
           <div className="width-[650px] height-[150px]">
             <button
             id="eerstebehandeling" value="Eerstegekozenbehandeling"
-            className="bg-white w-[650px]"
+            className="bg-white w-[650px] hover:border-2"
               
-              onClick={handleClick}>
+              onClick={() =>handleClick("behandeling1")}>
             
               <img
                 className="float-left rounded-l-lg"
@@ -78,8 +60,8 @@ function Reservering2(){
             </button>
           </div>
           <div className="width-[650px]">
-            <button id="tweedeKapper" value="tweedegekozenkapper" className="w-[650px]" type="button"
-            onClick={handleClick2} >
+            <button id="tweedeKapper" value="tweedegekozenkapper" className="w-[650px] hover:border-2" type="button"
+            onClick={() =>handleClick("behandeling2")} >
               <img
                 className="float-left rounded-l-lg"
                 src="https://lirp.cdn-website.com/c56c38f9/dms3rep/multi/opt/Ivar+Fotografie-12-702w.jpg"
@@ -93,8 +75,8 @@ function Reservering2(){
             </button>
           </div>
           <div className="width-[650px]">
-            <button className="w-[650px]" type="button" 
-            onClick={handleClick} >
+            <button className="w-[650px] hover:border-2" type="button" 
+            onClick={() =>handleClick("behandeling3")} >
               <img
                 className="float-left rounded-l-lg"
                 src="https://lirp.cdn-website.com/c56c38f9/dms3rep/multi/opt/Ivar+Fotografie-12-702w.jpg"
@@ -108,8 +90,8 @@ function Reservering2(){
             </button>
           </div>
           <div className="knop1">
-            <button className="w-[650px]" type="button"
-            onClick={handleClick} >
+            <button className="w-[650px] hover:border-2" type="button"
+            onClick={() =>handleClick("behandeling4")} >
               <img
                 className="float-left rounded-l-lg"
                 src="https://lirp.cdn-website.com/c56c38f9/dms3rep/multi/opt/Ivar+Fotografie-12-702w.jpg"
@@ -126,6 +108,7 @@ function Reservering2(){
       </h1>
     </div>
   </div>
+</div>
 </div>
 
         </>
