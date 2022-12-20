935fcd3966c4ffd51af9e718ad8b63125d67c770
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-12-20 09:29:37 +0100

    fix button the same sizes

--- pages/dashboard/kappers.tsx
+++ pages/dashboard/kappers.tsx
@@ -52,7 +52,8 @@ function AddHairdresserModal({
 							{/*footer*/}
 							<div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
 								<button
-									className="text-red-400 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none hover:ring-1 ring-offset-2 mr-1 mb-1 ease-linear transition-all rounded hover:ring-red-500 duration-150"
+									className="text-red-400 background-transparent font-bold uppercase px-6 py-3 text-sm outline-none hover:ring-1 
+ mr-1 mb-1 ease-linear transition-all rounded hover:ring-red-500 duration-150"
 									type="button"
 									onClick={() => setState(false)}
 								>
